81050f9ece33c100031ca75bce9dd4d8a54a7416
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-13 16:42:45 +0800

    Adds validation for input

--- 4.1_century_from_year.rb
+++ 4.1_century_from_year.rb
@@ -1,9 +1,15 @@
 def year_to_century(year)
-  result = year % 100 == 0 ? (year / 100) : (year / 100 + 1)
-  puts "centuryFromYear(#{year}) => #{result}"
+  if year <= 0
+    puts "centuryFromYear(#{year}) => Input is 0 or negative. Please enter a valid year."
+  else
+    result = year % 100 == 0 ? (year / 100) : (year / 100 + 1)
+    puts "centuryFromYear(#{year}) => #{result}th century"
+  end
 end
 
 #test
+year_to_century(0)
+year_to_century(-1900)
 year_to_century(1705)
 year_to_century(1900)
 year_to_century(1601)
